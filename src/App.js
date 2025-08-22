c408dd95a6446c08328a18d5f13d0b0a853c4b54
reymardequito <dequitoreymar22@gmail.com>
2025-08-21 23:26:04 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          piolo 
+         jericho babae dor 
         </p>
         <a
           className="App-link"
